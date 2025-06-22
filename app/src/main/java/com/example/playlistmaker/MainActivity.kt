677e1cb29e83998bc413a4145132e23f47be407f
Anasty-Sia <anastasiasizonenkoo@yandex.ru>
2025-06-22 18:34:04 +0300

    Исправление замечаний

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,39 +2,50 @@ package com.example.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val searchButton = findViewById<LinearLayout>(R.id.search_b)
+        val toolbar = findViewById<MaterialToolbar>(R.id.main_toolbar)
+        ViewCompat.setOnApplyWindowInsetsListener(toolbar) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                topMargin = systemBars.top
+            }
+            insets
+        }
+
+        val searchButton = findViewById<Button>(R.id.search_b)
 
         searchButton.setOnClickListener {
             val displayIntent = Intent(this, SearchActivity::class.java)
             startActivity(displayIntent)
         }
 
-        val libraryButton = findViewById<LinearLayout>(R.id.library_b)
+        val libraryButton = findViewById<Button>(R.id.library_b)
 
         libraryButton.setOnClickListener {
             val displayIntent = Intent(this, LibraryActivity::class.java)
             startActivity(displayIntent)
         }
 
-        val settingButton = findViewById<LinearLayout>(R.id.settings_b)
+        val settingButton = findViewById<Button>(R.id.settings_b)
 
         settingButton.setOnClickListener {
             val displayIntent = Intent(this, SettingsActivity::class.java)
             startActivity(displayIntent)
         }
-
-
-
-
-
 
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,26 +1,35 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
-import android.widget.Button
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
+
+            enableEdgeToEdge()
             setContentView(R.layout.activity_settings)
 
-            val backButton = findViewById<Button>(R.id.back_b)
+            val backButton = findViewById<MaterialToolbar>(R.id.back_b)
 
-            backButton.setOnClickListener {
+            ViewCompat.setOnApplyWindowInsetsListener(backButton) { view, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                    topMargin = systemBars.top
+                }
+                insets
+            }
+
+            backButton.setNavigationOnClickListener {
                 finish()
             }
         }
 
-
-
-
-
-
-
-
 }
+
